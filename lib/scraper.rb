97298151f0d1fab2dfd059c5ad6307fbe59cc9bc
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:21:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,6 +28,7 @@ class Scraper
     elsif element.attr("href").include?("github")
       return_hash[:github] = element.attr('href')
     elsif element.attr("href").end_with?("com/")
+      
 
   end
 
