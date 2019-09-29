5f99ce0954f6da01b73dcfcca07466350be7df8b
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:34:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     self.send("name=", student_hash)[:name])
-self.send()
+    self.send()
   end
 
   def self.create_from_collection(students_array)
