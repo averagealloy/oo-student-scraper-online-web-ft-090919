ce21a5bbbeeda26bde7b36e4ab39fcf5d796777f
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:14:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
     return_hash = {}
 
   social = doc.css(",vitals-container .social-icon-container a")
-  social.each do |element| #iderate thru asign 
+  social.each do |element| #iderate thru asign keys to ele
 
 
   end
