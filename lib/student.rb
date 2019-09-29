1486b6992d8d395159edfbcfe9a7d3c64e4f0e28
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:43:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
   def add_student_attributes(attributes_hash)
 self.send("twitter=", attributes_hash[:twitter])
 self.send("Linkedin=", attributes_hash[:Linkedin])
-self.send("github=", attributes_hash[:twitter])
+self.send("github=", attributes_hash[:t])
   end
 
   def self.all
