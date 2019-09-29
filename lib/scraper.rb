5b3f281926196109335601a571c00595dfcb7caf
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:04:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = open(index_url)
-    doc = Nokogiri :: HTM
+    doc = Nokogiri :: HTML
   end
 
   def self.scrape_profile_page(profile_url)
