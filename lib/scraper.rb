e35874fd00e687b70d2ac856f842183ee35ad082
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:05:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
     html = open(index_url)
     doc = Nokogiri :: HTML(html)
     student_cards = doc.css(".student-card a")
-    stude
+    student
   end
 
   def self.scrape_profile_page(profile_url)
