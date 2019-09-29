85938139a4456a90035d93a2ac01c2ea548ac2fb
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:26:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,4 +34,4 @@ class Scraper
 
 end
 return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profle-quote").text
-return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder")
+return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p")
