45ba94bb8075a762bd4069ea09747f36e8ebac00
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 14:02:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ class Student
     self.send("twitter=", attributes_hash[:twitter])
     self.send("linkedin=", attributes_hash[:linkedin])
     self.send("github=", attributes_hash[:github])
-    self.send("bloog=", attributes_hash[:blog])
+    self.send("blog=", attributes_hash[:blog])
     self.send("profile_quote=", attributes_hash[:profile])
     self.send("bio=", attributes_hash[:bio])
   end
