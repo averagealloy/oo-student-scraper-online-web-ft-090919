07a24ce3542df669a8e66b57ec95efb56a6b4930
Mike McTighe <m.mctighe10@gmail.com>
2019-09-29 13:12:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
     doc = Nokogiri :: HTML(html)
     return_hash = {}
 
-  social = doc.css(",vitals-container .social-icon-cont")
+  social = doc.css(",vitals-container .social-icon-container ")
 
 
   end
